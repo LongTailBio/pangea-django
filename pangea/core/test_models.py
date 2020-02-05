81c2c0adddaeb13b09cdf24d8450585caf02e198
David Danko <dcdanko@gmail.com>
2020-02-05 09:55:26 -0500

    Port sample tests over

---
+++ pangea/core/test_models.py
@@ -0,0 +1,54 @@
+"""Test suite for Sample model."""
+
+from django.test import TestCase
+
+from .models import (
+    Organization,
+    SampleGroup,
+    Sample,
+    SampleAnalysisResult,
+    SampleGroupAnalysisResult,
+    SampleAnalysisResultField,
+    SampleGroupAnalysisResultField,
+)
+
+
+class TestSampleModel(TestCase):
+    """Test suite for Sample model."""
+
+    def test_add_sample(self):
+        """Ensure sample model is created correctly."""
+        org = Organization.object.create(name='an_org AFFFS')
+        library = SampleGroup.object.create(
+            organization=org,
+            name='library AFFFS',
+            is_library=True,
+        )
+        sample = Sample.object.create(
+            name='SMPL_01 AFFFS',
+            library=library,
+            metadata={'foo': 'bar'}
+        )
+
+        self.assertTrue(sample.uuid)
+        self.assertEqual(sample.name, 'SMPL_01 AFFFS')
+        self.assertEqual(sample.metadata['foo'], 'bar')
+        self.assertTrue(sample.created_at)
+
+    def test_add_duplicate_name(self):
+        """Ensure duplicate sample names are not allowed."""
+        org = Organization.object.create(name='an_org OIUO')
+        lib = org.create_sample_group(name='LBRY_01 OIUO', is_library=True)
+        lib.create_sample(name='SMPL_01 OIUO')
+        dupe = lambda: lib.create_sample(name='SMPL_01 OIUO')
+        self.assertRaises(Exception, dupe)
+
+    def test_different_libraries(self):
+        """Ensure duplicate sample names in different libraries are allowed."""
+        org = Organization.object.create(name='an_org UIY')
+        lib1 = org.create_sample_group(name='LBRY_01 UIY', is_library=True)
+        lib2 = org.create_sample_group(name='LBRY_02 UIY', is_library=True)
+        original = lib1.create_sample(name='SMPL_01 UIY')
+        duplicate = lib2.create_sample(name='SMPL_01 UIY')
+        self.assertEqual(original.name, duplicate.name)
+        self.assertNotEqual(original.library.uuid, duplicate.library.uuid)
