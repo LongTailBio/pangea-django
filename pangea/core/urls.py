795022858806f778cb4b8d238822c40ecf862fc4
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-02-09 13:46:40 -0500

    refactor: give api routes distinct names

--- pangea/core/urls.py
+++ pangea/core/urls.py
@@ -12,16 +12,16 @@ from .views import (
 
 
 urlpatterns = {
-    path('organizations', OrganizationCreateView.as_view(), name="create"),
-    path('organizations/<uuid:pk>', OrganizationDetailsView.as_view(), name="details"),
-    path('sample_groups', SampleGroupCreateView.as_view(), name="create"),
-    path('sample_groups/<uuid:pk>', SampleGroupDetailsView.as_view(), name="details"),
-    path('samples', SampleCreateView.as_view(), name="create"),
-    path('samples/<uuid:pk>', SampleDetailsView.as_view(), name="details"),
-    path('sample_ars', SampleAnalysisResultCreateView.as_view(), name="create"),
-    path('sample_ars/<uuid:pk>', SampleAnalysisResultDetailsView.as_view(), name="details"),
-    path('sample_group_ars', SampleGroupAnalysisResultCreateView.as_view(), name="create"),
-    path('sample_group_ars/<uuid:pk>', SampleGroupAnalysisResultDetailsView.as_view(), name="details"),
+    path('organizations', OrganizationCreateView.as_view(), name="organization-create"),
+    path('organizations/<uuid:pk>', OrganizationDetailsView.as_view(), name="organization-details"),
+    path('sample_groups', SampleGroupCreateView.as_view(), name="sample-group-create"),
+    path('sample_groups/<uuid:pk>', SampleGroupDetailsView.as_view(), name="sample-group-details"),
+    path('samples', SampleCreateView.as_view(), name="sample-create"),
+    path('samples/<uuid:pk>', SampleDetailsView.as_view(), name="sample-details"),
+    path('sample_ars', SampleAnalysisResultCreateView.as_view(), name="sample-ars-create"),
+    path('sample_ars/<uuid:pk>', SampleAnalysisResultDetailsView.as_view(), name="sample-ars-details"),
+    path('sample_group_ars', SampleGroupAnalysisResultCreateView.as_view(), name="sample-group-ars-create"),
+    path('sample_group_ars/<uuid:pk>', SampleGroupAnalysisResultDetailsView.as_view(), name="sample-group-ars-details"),
 }
 
 
