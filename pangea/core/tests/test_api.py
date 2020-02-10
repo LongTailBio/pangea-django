903efa241ad7099a9cf139784c2d4630e43d968f
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-02-10 07:01:42 -0500

    refactor(tests): use setUpTestData

--- pangea/core/tests/test_api.py
+++ pangea/core/tests/test_api.py
@@ -2,10 +2,19 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from pangea.core.models import PangeaUser, Organization, SampleGroup
+from pangea.core.models import (
+    PangeaUser,
+    Organization,
+    SampleGroup,
+    SampleLibrary,
+)
 
 
 class OrganizationTests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = PangeaUser.objects.create(email='user@domain.com', password='Foobar22')
 
     def test_create_unauthed_organization(self):
         """Ensure 403 error is throw when trying to create organization if unauthed."""
@@ -18,8 +27,7 @@ class OrganizationTests(APITestCase):
 
     def test_create_organization(self):
         """Ensure authenticated user can create organization."""
-        user = PangeaUser.objects.create(email='user@domain.com',password='Foobar22')
-        self.client.force_authenticate(user=user)
+        self.client.force_authenticate(user=self.user)
 
         url = reverse('organization-create')
         data = {'name': 'Test Organization'}
@@ -28,16 +36,20 @@ class OrganizationTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Organization.objects.count(), 1)
         self.assertEqual(Organization.objects.get().name, 'Test Organization')
-        self.assertIn(user, Organization.objects.get().users.all())
+        self.assertIn(self.user, Organization.objects.get().users.all())
 
 
 class SampleGroupTests(APITestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.organization = Organization.objects.create(name='Test Organization')
+        cls.user = PangeaUser.objects.create(email='user@domain.com', password='Foobar22')
+
     def test_create_unauthenticated_sample_group(self):
         """Ensure 403 error is throw when trying to create sample group if unauthenticated."""
-        organization = Organization.objects.create(name='Test Organization')
         url = reverse('sample-group-create')
-        data = {'name': 'Test Sample Group', 'organization': organization.pk}
+        data = {'name': 'Test Sample Group', 'organization': self.organization.pk}
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -45,12 +57,10 @@ class SampleGroupTests(APITestCase):
 
     def test_create_unauthorized_sample_group(self):
         """Ensure 403 error is throw when trying to create sample group if unauthorized."""
-        user = PangeaUser.objects.create(email='user@domain.com',password='Foobar22')
-        self.client.force_authenticate(user=user)
+        self.client.force_authenticate(user=self.user)
 
-        organization = Organization.objects.create(name='Test Organization')
         url = reverse('sample-group-create')
-        data = {'name': 'Test Sample Group', 'organization': organization.pk}
+        data = {'name': 'Test Sample Group', 'organization': self.organization.pk}
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -58,13 +68,11 @@ class SampleGroupTests(APITestCase):
 
     def test_create_sample_group(self):
         """Ensure authorized user can create sample group."""
-        organization = Organization.objects.create(name='Test Organization')
-        user = PangeaUser.objects.create(email='user@domain.com',password='Foobar22')
-        organization.users.add(user)
-        self.client.force_authenticate(user=user)
+        self.organization.users.add(self.user)
+        self.client.force_authenticate(user=self.user)
 
         url = reverse('sample-group-create')
-        data = {'name': 'Test Sample Group', 'organization': organization.pk}
+        data = {'name': 'Test Sample Group', 'organization': self.organization.pk}
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
