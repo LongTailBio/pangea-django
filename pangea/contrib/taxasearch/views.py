2a6b491d6b4cabf56e2884074a11a733a33b7a13
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-02-11 06:49:13 -0500

    feat: group results by taxa
    It may make sense to switch to json_build_object() in future. See:
    https://dba.stackexchange.com/a/69658

--- pangea/contrib/taxasearch/views.py
+++ pangea/contrib/taxasearch/views.py
@@ -1,10 +1,17 @@
+from django.db import connection
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 
-from pangea.core.models import Sample
+
+def search_result(raw_result):
+    """Map query result to something serializable."""
+    return {
+        'taxa': raw_result[0],
+        'samples': raw_result[1],
+    }
 
 
 @api_view(['GET'])
@@ -14,47 +21,51 @@ def fuzzy_taxa_search(request):
     if query is None:
         raise ValidationError(_('Must provide URL-encoded `query` query parameter.'))
 
-
-    query = f'%{query}%'
-    results = Sample.objects.raw('''
-        -- Use text-based search to restrict the search space
-        with clearcut as (
-            select analysis_result_id, stored_data
-            from core_sampleanalysisresultfield
-            where name = 'relative_abundance'
-                and stored_data::text ilike %s
-        ),
-        -- Search on actual taxa results
-        filtered_taxa as (
+    with connection.cursor() as cursor:
+        query = f'%{query}%'
+        cursor.execute('''
+            -- Use text-based search to restrict the search space
+            with clearcut as (
+                select analysis_result_id, stored_data
+                from core_sampleanalysisresultfield
+                where name = 'relative_abundance'
+                    and stored_data::text ilike %s
+            ),
+            -- Search on actual taxa results
+            filtered_taxa as (
+                select
+                    clearcut.analysis_result_id,
+                    taxa.*
+                from
+                    clearcut,
+                    jsonb_each_text(clearcut.stored_data) as taxa
+                where
+                    taxa.key ilike %s
+            )
+            -- Pull in Sample records
             select
-                clearcut.analysis_result_id,
-                taxa.*
+                filtered_taxa.key as taxa,
+                json_agg((select x from (
+                    select
+                        filtered_taxa.value::float as relative_abundance,
+                        core_sample.uuid as sample_uuid,
+                        core_sample.name as sample_name,
+                        core_sample.library_id as sample_library_uuid,
+                        core_sample.metadata as sample_metadata
+                    order by
+                        core_sample.library_id
+                ) as x)) as samples
             from
-                clearcut,
-                jsonb_each_text(clearcut.stored_data) as taxa
-            where
-                taxa.key ilike %s
-        )
-        -- Pull in Sample records
-        select
-            core_sample.*,
-            filtered_taxa.key as taxa,
-            filtered_taxa.value::float as abundance
-        from
-            core_sample
-            join core_sampleanalysisresult
-                on core_sampleanalysisresult.sample_id = core_sample.uuid
-            join filtered_taxa
-                on filtered_taxa.analysis_result_id = core_sampleanalysisresult.uuid
-        ''', [query, query])
+                core_sample
+                join core_sampleanalysisresult
+                    on core_sampleanalysisresult.sample_id = core_sample.uuid
+                join filtered_taxa
+                    on filtered_taxa.analysis_result_id = core_sampleanalysisresult.uuid
+            group by
+                filtered_taxa.key
+            order by
+                filtered_taxa.key
+            ''', [query, query])
 
-    def search_result(sample):
-        """Map query result to something serializable."""
-        return {
-            'uuid': sample.uuid,
-            'name': sample.name,
-            'taxa': sample.taxa,
-            'abundance': sample.abundance,
-        }
-
-    return Response({'results': [search_result(result) for result in results]})
+        results = [search_result(row) for row in cursor.fetchall()]
+        return Response({'results': results})
