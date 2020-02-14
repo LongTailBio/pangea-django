22f09bc13c6bbb3531e41ac2801d16f20acde586
David Danko <dcdanko@gmail.com>
2020-02-14 11:25:12 -0500

    fix: return autoupdatemixin on save, attempt 3

--- pangea/core/mixins.py
+++ pangea/core/mixins.py
@@ -34,9 +34,8 @@ class AutoCreatedUpdatedMixin(models.Model):
             auto_updated_at_is_disabled = kwargs.pop('disable_auto_updated_at', False)
             if not auto_updated_at_is_disabled:
                 self.updated_at = now()
-        out = super(AutoCreatedUpdatedMixin, self)
-        out.save(*args, **kwargs)
-        return out
+        super(AutoCreatedUpdatedMixin, self).save(*args, **kwargs)
+        return self
 
 
 class SoftDeleteMixin(models.Model):
