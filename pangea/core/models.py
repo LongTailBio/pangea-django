c7dcfd5d52d143b16d204fd8ba68de12d7bb82c3
David Danko <dcdanko@gmail.com>
2020-02-19 11:55:14 -0500

    feat(organizations): log users as strings

--- pangea/core/models.py
+++ pangea/core/models.py
@@ -41,7 +41,7 @@ class Organization(AutoCreatedUpdatedMixin):
             obj_uuid=self.uuid,
             saved_uuid=out.uuid,
             name=self.name,
-            users=self.users,
+            users=[str(user) for user in self.users],
         )
         return out
 
