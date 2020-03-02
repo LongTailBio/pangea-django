ae1dc634d60a24c3af9a430d6387d8069ec6ccc4
David Danko <dcdanko@gmail.com>
2020-03-02 13:02:00 -0500

    feat: add more armature and boilerplate

---
+++ api-client/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools
+setuptools.setup(
+    name='capalyzer',
+    version='2.16.1',
+    description="Parsing functionality for the metasub CAP",
+    author="David C. Danko",
+    author_email='dcdanko@gmail.com',
+    url='https://github.com/dcdanko/capalyzer',
+    packages=setuptools.find_packages(),
+    package_dir={'capalyzer': 'capalyzer'},
+    install_requires=[
+        'click',
+        'pandas',
+        'scipy',
+        'numpy',
+        'umap-learn',
+    ],
+    entry_points={
+        'console_scripts': [
+            'capalyzer=capalyzer.cli:main'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+    package_data={'capalyzer': [
+        'packet_parser/ncbi_tree/*.dmp.gz',
+        'packet_parser/microbe-directory.csv',
+    ]},
+)
