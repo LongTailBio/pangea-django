d854d17c49a343eedce03ec6d93deb06a40cbcf4
David Danko <dcdanko@gmail.com>
2020-03-02 12:45:54 -0500

    feat: python package to interface with a pangea remote

---
+++ api-client/pangea-client/organization.py
@@ -0,0 +1,28 @@
+
+from .remote_object import RemoteObject
+from .sample_group import SampleGroup
+
+
+class Organization(RemoteObject):
+
+    def __init__(self, knex, name):
+        super().__init__(self)
+        self.knex = knex
+        self.name
+
+    def load_blob(self, blob):
+        self.uuid = blob['uuid']
+        self.created_at = blob['created_at']
+        self.updated_at = blob['updated_at']
+
+    def _get(self):
+        """Fetch the result from the server."""
+        blob = self.knex.get(f'nested/{self.name}')
+        self.load_blob(blob)
+
+    def _create(self):
+        blob = self.knex.post(f'organizations?format=json', json={'name': name})
+        self.load_blob(blob)
+
+    def sample_group(self, group_name, is_library=False):
+        return SampleGroup(self.knex, self, group_name, is_library=is_library)
