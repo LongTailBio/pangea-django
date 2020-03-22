8ca241650dec526f9d17f86c3a99201079270505
David Danko <dcdanko@gmail.com>
2020-03-22 16:46:10 -0400

    feat(covid19): auto download database

--- pangea/contrib/covid19/bio.py
+++ pangea/contrib/covid19/bio.py
@@ -1,19 +1,38 @@
 
 import subprocess as sp
 from os import environ
+from os.path import isdir
 
 KRAKEN2_EXC = 'kraken2'
-KRAKEN2_DB = environ['COVID19_KRAKEN2_DB']
+KRAKEN2_DB = environ.get('COVID19_KRAKEN2_DB', None)
 THREADS = int(environ.get('COVID19_THREADS', 1))
+KRAKEN2_DB_URL = 'https://s3.wasabisys.com/metasub/covid/kraken2_covid_2020_03_13.tar.gz'
+
+
+def download_kraken2():
+    """Download a custom Kraken2 database for detecting COVID."""
+    local_path = f'covid19/dbs/{KRAKEN2_DB_URL.split("/")[-1].split('.tar.gz')[0]}'
+    if isdir(local_path):
+        return local_path
+    cmd = (
+        f'cd covid19/dbs/ && '
+        f'wget {KRAKEN2_DB_URL} && '
+        f'tar -xzf {KRAKEN2_DB_URL.split("/")[-1]} '
+    )
+    sp.check_call(cmd, shell=True)
+    return local_path
 
 
 def kraken2_search_reads(reads, outprefix):
     """Use Kraken2 to make a fast pass report on reads. Write report to outfile."""
     reads = abspath(reads)
     report_filepath = f'{outprefix}.kraken2_report'
+    kraken2_db = KRAKEN2_DB
+    if kraken2_db is None:
+        kraken2_db = download_kraken2()
     cmd = (
         f'{KRAKEN2_EXC} '
-        f'--db {KRAKEN2_DB} '
+        f'--db {kraken2_db} '
         f'--threads {THREADS} '
         f'--report {report_filepath} '
         f'--report-zero-counts '
