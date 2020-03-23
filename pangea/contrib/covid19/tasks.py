8e9d47e310eb95135ce0a9586d2b93ec915873c8
David Danko <dcdanko@gmail.com>
2020-03-22 23:06:19 -0400

    feat: more descriptive filename

--- pangea/contrib/covid19/tasks.py
+++ pangea/contrib/covid19/tasks.py
@@ -23,9 +23,9 @@ def process_covid19(user_id, reads_path):
     with cloud_file_path(reads_path) as temp_file_path:
         logger.info('fetched_temp_cloud_storage_file', temp_file_path=temp_file_path)
 
-        results_object_name = f'covid19/results/{user_id}.txt'
         result_filepath = kraken2_search_reads(temp_file_path, f'{user_id}_covid19_results')
         logger.info('ran_kraken2', result_filepath=result_filepath)
+        results_object_name = f'covid19/results/{result_filepath}'
         upload_file(result_filepath, object_name=results_object_name)
 
         # One week expiration
