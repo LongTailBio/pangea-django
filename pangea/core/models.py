f789ea215b86b4eda590d9068d8cf21d19be7ef0
David C Danko <dcdanko@gmail.com>
2020-03-24 22:28:54 -0400

    Update pangea/core/models.py
    Co-Authored-By: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>

--- pangea/core/models.py
+++ pangea/core/models.py
@@ -1,6 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
 from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import JSONField
 from django.db import models
