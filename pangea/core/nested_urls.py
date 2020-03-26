247c3a6817ad2b53e56a346868e0973aff34dbe3
David C Danko <dcdanko@gmail.com>
2020-03-25 22:49:30 -0400

    Feature: Add Nested URL Patterns (#23)
    * feat(nested-urls): add preliminary nested URL routers
    
    * feat: sampel group create working as nested url
    
    * feat: fully supported and tested nested urls
    
    * chore: we do not actually use django rest framework
    
    * update reqs
    
    * Update pangea/core/nested_urls.py
    
    Co-Authored-By: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
    
    * Update pangea/core/nested_urls.py
    
    Co-Authored-By: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
    
    * Update pangea/core/nested_urls.py
    
    Co-Authored-By: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
    
    * Update pangea/core/nested_urls.py
    
    Co-Authored-By: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
    
    * feat: add some retrieval tests for nested urls
    
    * fix: some bugs affecting object creation through nested urls
    
    Co-authored-by: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>

---
+++ pangea/core/nested_urls.py
@@ -0,0 +1,187 @@
+'''
+Additional URLs that support nested access and access by name.
+
+/{org_pk}/
+/{org_pk}/sample_groups/
+/{org_pk}/sample_groups/{grp_pk}/
+/{org_pk}/sample_groups/{grp_pk}/analysis_results
+/{org_pk}/sample_groups/{grp_pk}/analysis_results/{ar_pk}
+/{org_pk}/sample_groups/{grp_pk}/analysis_results/{ar_pk}/fields
+/{org_pk}/sample_groups/{grp_pk}/analysis_results/{ar_pk}/fields/{field_pk}
+/{org_pk}/sample_groups/{grp_pk}/samples/
+/{org_pk}/sample_groups/{grp_pk}/samples/{sample_pk}/
+/{org_pk}/sample_groups/{grp_pk}/samples/{sample_pk}/analysis_results
+/{org_pk}/sample_groups/{grp_pk}/samples/{sample_pk}/analysis_results/{ar_pk}
+/{org_pk}/sample_groups/{grp_pk}/samples/{sample_pk}/analysis_results/{ar_pk}/fields
+/{org_pk}/sample_groups/{grp_pk}/samples/{sample_pk}/analysis_results/{ar_pk}/fields/{field_pk}
+'''
+
+from django.urls import path
+from uuid import UUID
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from .views import (
+    OrganizationDetailsView,
+    SampleGroupCreateView, SampleGroupDetailsView,
+    SampleCreateView, SampleDetailsView,
+    SampleAnalysisResultCreateView, SampleAnalysisResultDetailsView,
+    SampleGroupAnalysisResultCreateView, SampleGroupAnalysisResultDetailsView,
+    SampleAnalysisResultFieldCreateView, SampleAnalysisResultFieldDetailsView,
+    SampleGroupAnalysisResultFieldCreateView, SampleGroupAnalysisResultFieldDetailsView,
+)
+from .models import (
+    Organization,
+    SampleGroup,
+    Sample,
+    SampleAnalysisResult,
+    SampleGroupAnalysisResult,
+    SampleAnalysisResultField,
+    SampleGroupAnalysisResultField,
+)
+
+
+def is_uuid(el):
+    """Return true if el is an UUID."""
+    try:
+        UUID(el)
+        return True
+    except ValueError:
+        return False
+
+
+def to_uuid(**kwargs):
+    """Return a UUID and a field name for the lowest parent level in the URL."""
+    # Keys are:
+    # - named path parameter
+    # - name of the model's foreign key to its parent
+    # - model
+    # - name of parent id parameter in create payload
+    keys = [('grp_pk', 'organization', SampleGroup, 'sample_group')]
+    # Identify which routing branch was taken based on presence of named path params
+    if 'sample_pk' in kwargs:
+        keys += [
+            ('sample_pk', 'library', Sample, 'sample'),
+            ('ar_pk', 'sample', SampleAnalysisResult, 'analysis_result'),
+            ('field_pk', 'analysis_result', SampleAnalysisResultField, None),
+        ]
+    else:
+        keys += [
+            ('ar_pk', 'sample_group', SampleGroupAnalysisResult, 'analysis_result'),
+            ('field_pk', 'analysis_result', SampleGroupAnalysisResultField, None),
+        ]
+    org_key = kwargs['org_pk']
+    parent_field_name = 'organization'
+    if is_uuid(org_key):
+        parent = Organization.objects.get(pk=org_key)
+    else:
+        parent = Organization.objects.get(name=org_key)
+
+    # Traverse down through whichever path segments present in the request
+    for uuid_key, parent_key_name, model, field_name in keys:
+        if uuid_key not in kwargs:
+            break
+        filter_field = 'pk' if is_uuid(kwargs[uuid_key]) else 'name'
+        parent = model.objects.get(**{
+            parent_key_name: parent.uuid,
+            filter_field: kwargs[uuid_key],
+        })
+        parent_field_name = field_name
+    return parent.uuid, parent_field_name
+
+
+def nested_path(url, base_view, *out_args, **out_kwargs):
+    """Return a path with an intercepted view function."""
+    create = out_kwargs.pop('create', False)
+
+    def my_request(request, *args, **kwargs):
+        """Return the result of the base view function with a modified result."""
+        uuid_kwargs = {}
+        for key in ['org_pk', 'grp_pk', 'sample_pk', 'ar_pk', 'field_pk']:
+            if key in kwargs:
+                uuid_kwargs[key] = kwargs.pop(key)
+        uuid, field_name = to_uuid(**uuid_kwargs)
+        if create:
+            post = request.POST.copy()
+            post[field_name] = uuid
+            request.POST = post
+        else:
+            kwargs['pk'] = uuid
+        return base_view(request, *args, **kwargs)
+
+    return path(url, my_request, *out_args, **out_kwargs)
+
+
+urlpatterns = {
+    nested_path(
+        '<org_pk>/',
+        OrganizationDetailsView.as_view(),
+        name="nested-organization-detail"
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/',
+        SampleGroupCreateView.as_view(),
+        create=True,
+        name="nested-sample-group-create"
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/',
+        SampleGroupDetailsView.as_view(),
+        name="nested-sample-group-detail"
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/analysis_results',
+        SampleGroupAnalysisResultCreateView.as_view(),
+        create=True,
+        name='nested-sample-group-ar-create',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/analysis_results/<ar_pk>',
+        SampleGroupAnalysisResultDetailsView.as_view(),
+        name='nested-sample-group-ar-details',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/analysis_results/<ar_pk>/fields',
+        SampleGroupAnalysisResultFieldCreateView.as_view(),
+        create=True,
+        name='nested-sample-group-ar-field-create',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/analysis_results/<ar_pk>/fields/<field_pk>',
+        SampleGroupAnalysisResultFieldDetailsView.as_view(),
+        name='nested-sample-group-ar-field-details',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/samples/',
+        SampleCreateView.as_view(),
+        create=True,
+        name='nested-sample-create',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/samples/<sample_pk>/',
+        SampleDetailsView.as_view(),
+        name='nested-sample-details',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/samples/<sample_pk>/analysis_results',
+        SampleAnalysisResultCreateView.as_view(),
+        create=True,
+        name='nested-sample-ar-create',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/samples/<sample_pk>/analysis_results/<ar_pk>',
+        SampleAnalysisResultDetailsView.as_view(),
+        name='nested-sample-ar-details',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/samples/<sample_pk>/analysis_results/<ar_pk>/fields',
+        SampleAnalysisResultFieldCreateView.as_view(),
+        create=True,
+        name='nested-sample-ar-field-create',
+    ),
+    nested_path(
+        '<org_pk>/sample_groups/<grp_pk>/samples/<sample_pk>/analysis_results/<ar_pk>/fields/<field_pk>',
+        SampleAnalysisResultFieldDetailsView.as_view(),
+        name='nested-sample-ar-field-details',
+    )
+}
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ pangea/core/tests/test_nested_api.py
@@ -0,0 +1,194 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from pangea.core.models import (
+    PangeaUser,
+    Organization,
+    SampleGroup,
+    SampleLibrary,
+    Sample,
+    SampleGroupAnalysisResult,
+    SampleAnalysisResult,
+)
+
+
+class NestedSampleGroupTests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.organization = Organization.objects.create(name='Test Organization HJDH')
+        cls.user = PangeaUser.objects.create(email='user@domain.com', password='Foobar22')
+
+    def test_create_sample_group(self):
+        """Ensure authorized user can create sample group."""
+        self.organization.users.add(self.user)
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse('nested-sample-group-create', kwargs={'org_pk': self.organization.pk})
+        data = {'name': 'Test Sample Group HJKHJ', 'organization': self.organization.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SampleGroup.objects.count(), 1)
+        self.assertEqual(SampleGroup.objects.get().name, 'Test Sample Group HJKHJ')
+
+    def test_create_sample_group_with_name(self):
+        """Ensure authorized user can create sample group."""
+        self.organization.users.add(self.user)
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse('nested-sample-group-create', kwargs={'org_pk': self.organization.name})
+        data = {'name': 'Test Sample Group NBYTU', 'organization': self.organization.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SampleGroup.objects.count(), 1)
+        self.assertEqual(SampleGroup.objects.get().name, 'Test Sample Group NBYTU')
+
+    def test_retrieve_sample_group(self):
+        """Ensure authorized user can create sample group."""
+        grp_name = 'Test Sample Group HGJHJ'
+        self.organization.create_sample_group(name=grp_name, is_public=True)
+
+        url = reverse('nested-sample-group-detail', kwargs={
+            'org_pk': self.organization.pk,
+            'grp_pk': grp_name,
+        })
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class NestedSampleTests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.organization = Organization.objects.create(name='Test Organization EWKKK')
+        cls.user = PangeaUser.objects.create(email='user@domain.com', password='Foobar22')
+
+    def test_create_sample(self):
+        """Ensure authorized user can create sample group."""
+        self.organization.users.add(self.user)
+        self.client.force_authenticate(user=self.user)
+
+        sample_library = self.organization.create_sample_group(name='Test Library JKLLL', is_library=True)
+        url = reverse('nested-sample-create', kwargs={
+            'org_pk': self.organization.pk,
+            'grp_pk': sample_library.pk,
+        })
+        data = {'name': 'Test Sample JKLLL', 'library': sample_library.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Sample.objects.count(), 1)
+        self.assertEqual(Sample.objects.get().name, 'Test Sample JKLLL')
+        self.assertTrue(sample_library.sample_set.filter(pk=response.data.get('uuid')).exists())
+
+    def test_create_sample_with_name(self):
+        """Ensure authorized user can create sample group."""
+        self.organization.users.add(self.user)
+        self.client.force_authenticate(user=self.user)
+
+        sample_library = self.organization.create_sample_group(name='Test Library', is_library=True)
+        url = reverse('nested-sample-create', kwargs={
+            'org_pk': self.organization.name,
+            'grp_pk': sample_library.name,
+        })
+        data = {'name': 'Test Sample', 'library': sample_library.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Sample.objects.count(), 1)
+        self.assertEqual(Sample.objects.get().name, 'Test Sample')
+        self.assertTrue(sample_library.sample_set.filter(pk=response.data.get('uuid')).exists())
+
+    def test_retrieve_sample(self):
+        """Ensure authorized user can create sample group."""
+        grp = self.organization.create_sample_group(
+            name='Test Sample Group KKJSGHFG',
+            is_public=True,
+            is_library=True,
+        )
+        grp.create_sample(name='Test Sample KKJSGHFG')
+
+        url = reverse('nested-sample-details', kwargs={
+            'org_pk': self.organization.pk,
+            'grp_pk': grp.name,
+            'sample_pk': 'Test Sample KKJSGHFG',
+        })
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class AnalysisResultTests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.organization = Organization.objects.create(name='Test Organization')
+        cls.user = PangeaUser.objects.create(email='user@domain.com', password='Foobar22')
+        cls.organization.users.add(cls.user)
+        cls.sample_group = cls.organization.create_sample_group(name='Test Library', is_library=True)
+        cls.sample_library = cls.sample_group.library
+        cls.sample = Sample.objects.create(name='Test Sample', library=cls.sample_library)
+
+    def test_create_sample_group_analysis_result(self):
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse('nested-sample-group-ar-create', kwargs={
+            'org_pk': self.organization.pk,
+            'grp_pk': self.sample_library.pk,
+        })
+        data = {'module_name': 'taxa', 'sample_group': self.sample_group.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SampleGroupAnalysisResult.objects.count(), 1)
+        self.assertEqual(SampleGroupAnalysisResult.objects.get().sample_group, self.sample_group)
+        self.assertEqual(SampleGroupAnalysisResult.objects.get().module_name, 'taxa')
+
+    def test_create_sample_analysis_result(self):
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse('nested-sample-ar-create', kwargs={
+            'org_pk': self.organization.pk,
+            'grp_pk': self.sample_library.pk,
+            'sample_pk': self.sample.pk,
+        })
+        data = {'module_name': 'taxa', 'sample': self.sample.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SampleAnalysisResult.objects.count(), 1)
+        self.assertEqual(SampleAnalysisResult.objects.get().sample, self.sample)
+        self.assertEqual(SampleAnalysisResult.objects.get().module_name, 'taxa')
+
+    def test_create_sample_group_analysis_result_with_name(self):
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse('nested-sample-group-ar-create', kwargs={
+            'org_pk': self.organization.name,
+            'grp_pk': self.sample_library.group.name,
+        })
+        data = {'module_name': 'taxa', 'sample_group': self.sample_group.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SampleGroupAnalysisResult.objects.count(), 1)
+        self.assertEqual(SampleGroupAnalysisResult.objects.get().sample_group, self.sample_group)
+        self.assertEqual(SampleGroupAnalysisResult.objects.get().module_name, 'taxa')
+
+    def test_create_sample_analysis_result_with_name(self):
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse('nested-sample-ar-create', kwargs={
+            'org_pk': self.organization.name,
+            'grp_pk': self.sample_library.group.name,
+            'sample_pk': self.sample.name,
+        })
+        data = {'module_name': 'taxa', 'sample': self.sample.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SampleAnalysisResult.objects.count(), 1)
+        self.assertEqual(SampleAnalysisResult.objects.get().sample, self.sample)
+        self.assertEqual(SampleAnalysisResult.objects.get().module_name, 'taxa')
