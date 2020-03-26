ab1cb260a48c73d07f62b143974527f6454dc81f
David Danko <dcdanko@gmail.com>
2020-03-26 00:21:31 -0400

    feat: case insensitive matching in nested urls

--- pangea/core/tests/test_nested_api.py
+++ pangea/core/tests/test_nested_api.py
@@ -58,6 +58,19 @@ class NestedSampleGroupTests(APITestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_retrieve_sample_group_case_insensitive(self):
+        """Ensure authorized user can create sample group."""
+        grp_name = 'Test Sample Group TRUGKH'
+        self.organization.create_sample_group(name=grp_name, is_public=True)
+
+        url = reverse('nested-sample-group-detail', kwargs={
+            'org_pk': self.organization.pk,
+            'grp_pk': grp_name.lower(),
+        })
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
 
 class NestedSampleTests(APITestCase):
 
