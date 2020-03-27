73135a2922cd2193297c1bb9457e5f930762c2cd
David Danko <dcdanko@gmail.com>
2020-03-27 09:40:40 -0400

    chore: add localhost as ALLOWED_HOSTS by default

--- pangea/settings.py
+++ pangea/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'SomethingSecret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
