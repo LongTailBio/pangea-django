da3df67fc461f3d92c2bc473fddc85f46ad642b2
David Danko <dcdanko@gmail.com>
2020-03-27 09:40:06 -0400

    feature: actual client tests

--- api-client/tests/test_api_client.py
+++ api-client/tests/test_api_client.py
@@ -4,39 +4,30 @@ import pandas as pd
 
 from unittest import TestCase
 from os.path import join, dirname
+from os import environ
 
-from capalyzer.packet_parser import DataTableFactory
-from capalyzer.packet_parser.experimental import (
-    umap,
-    fractal_dimension,
-    pca_sample_cross_val,
+from pangea_api import (
+    Knex,
+    Sample,
+    Organization,
+    User,
 )
 
 PACKET_DIR = join(dirname(__file__), 'built_packet')
+ENDPOINT = environ.get('PANGEA_API_TESTING_ENDPOINT', 'http://127.0.0.1:8000')
+USERNAME = environ.get('PANGEA_API_TESTING_USERNAME', 'foo@bar.com')
+PASSWORD = environ.get('PANGEA_API_TESTING_PASSWORD', 'Foobar22')
 
 
 class TestPacketParser(TestCase):
     """Test suite for packet building."""
 
-    def test_umap(self):
-        """Test that we can run UMAP."""
-        taxa = DataTableFactory(PACKET_DIR).taxonomy()
-        taxa = pd.DataFrame(pd.concat([taxa, taxa, taxa]))
-        umap(taxa, n_neighbors=3)
+    def setUp(self):
+        self.knex = Knex(ENDPOINT)
+        self.user = User(self.knex, USERNAME, PASSWORD).register()
 
-    def test_fractal(self):
-        """Test that we can run fractal."""
-        taxa = DataTableFactory(PACKET_DIR).taxonomy()
-        taxa = pd.DataFrame(pd.concat([taxa, taxa, taxa]))
-        taxa = umap(taxa, n_neighbors=3)
-        fractal_dimension(taxa, scales=range(1, 3))
-
-    def test_pca_cross_val(self):
-        taxa = DataTableFactory(PACKET_DIR).taxonomy()
-        taxa = pd.DataFrame(pd.concat([taxa] * 10))
-        taxa_copy = taxa.copy(deep=True)
-        catch_losses = []  # demo of how to extract detailed loss info
-        taxa_pca = pca_sample_cross_val(taxa, comp_step=10, losses=catch_losses)
-        self.assertEqual(taxa.shape[0], taxa_pca.shape[0])
-        self.assertEqual(taxa.shape[1], taxa_pca.shape[1])
-        self.assertEqual((taxa - taxa_copy).sum().sum(), 0)
+    def test_create_org(self):
+        """Test that we can create a sample."""
+        org = Organization(self.knex, 'my_client_test_org')
+        org.create()
+        self.assertTrue(org.uuid)
