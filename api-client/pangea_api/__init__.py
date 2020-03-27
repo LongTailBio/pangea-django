88f30baa855f72517c6b24ed850ee53aac8cbce2
David Danko <dcdanko@gmail.com>
2020-03-27 09:38:52 -0400

    chore: rename pangea_client to pangea_api

---
+++ api-client/pangea_api/__init__.py
@@ -0,0 +1,5 @@
+
+from .knex import Knex
+from .sample import Sample
+from .organization import Organization
+from .user import User
--- api-client/setup.py
+++ api-client/setup.py
@@ -5,34 +5,18 @@ import setuptools
 
 setuptools
 setuptools.setup(
-    name='capalyzer',
-    version='2.16.1',
-    description="Parsing functionality for the metasub CAP",
+    name='pangea_api',
+    version='0.1.0',
     author="David C. Danko",
     author_email='dcdanko@gmail.com',
-    url='https://github.com/dcdanko/capalyzer',
     packages=setuptools.find_packages(),
-    package_dir={'capalyzer': 'capalyzer'},
+    package_dir={'pangea_api': 'pangea_api'},
     install_requires=[
-        'click',
-        'pandas',
-        'scipy',
-        'numpy',
-        'umap-learn',
     ],
-    entry_points={
-        'console_scripts': [
-            'capalyzer=capalyzer.cli:main'
-        ]
-    },
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
     ],
-    package_data={'capalyzer': [
-        'packet_parser/ncbi_tree/*.dmp.gz',
-        'packet_parser/microbe-directory.csv',
-    ]},
 )
