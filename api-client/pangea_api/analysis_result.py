f318f6812f738f31403685e41d0f8061561ea5b8
David C Danko <dcdanko@gmail.com>
2020-04-03 10:21:30 -0400

    chore: remove unused code cruft

--- api-client/pangea_api/analysis_result.py
+++ api-client/pangea_api/analysis_result.py
@@ -2,21 +2,6 @@
 from .remote_object import RemoteObject
 
 
-class ReadOnlyDict(dict):
-
-    def __readonly__(self, *args, **kwargs):
-        raise RuntimeError("Cannot modify ReadOnlyDict")
-
-    __setitem__ = __readonly__
-    __delitem__ = __readonly__
-    pop = __readonly__
-    popitem = __readonly__
-    clear = __readonly__
-    update = __readonly__
-    setdefault = __readonly__
-    del __readonly__
-
-
 class AnalysisResult(RemoteObject):
     remote_fields = [
         'uuid',
