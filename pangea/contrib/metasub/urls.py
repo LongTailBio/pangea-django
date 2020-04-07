974163d91e0cc82e707803bd43a2e62a71e9022b
David Danko <dcdanko@gmail.com>
2020-04-07 15:34:52 -0400

    fix: sumburst -> sunburst

--- pangea/contrib/metasub/urls.py
+++ pangea/contrib/metasub/urls.py
@@ -12,7 +12,7 @@ from .views import (
 urlpatterns = {
     path('search_samples', fuzzy_taxa_search_samples, name="metasub-sample-taxa-search"),
     path('search_cities', fuzzy_taxa_search_cities, name="metasub-city-taxa-search"),
-    path('sample_sumburst/<uuid:pk>', sample_taxonomy_sunburst, name='metasub-sample-sunburst'),
+    path('sample_sunburst/<uuid:pk>', sample_taxonomy_sunburst, name='metasub-sample-sunburst'),
 }
 
 
