e3b2e7b9b62d9bab4051bb0c51393bd35bff424d
David Danko <dcdanko@gmail.com>
2021-01-05 09:39:19 -0500

    chore: bump version

--- client-programs/contrib/metagenscope/setup.py
+++ client-programs/contrib/metagenscope/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name='metagenscope',
-    version='3.1.0',
+    version='3.1.1',
     author="David C. Danko",
     author_email='dcdanko@gmail.com',
     packages=setuptools.find_packages(),
