d981c593ebebce8a9d891ea5d9c1b468f5c47c6f
David Danko <dcdanko@gmail.com>
2021-05-11 19:49:51 -0400

    fix: validate metadata table with sample names

--- pangea/core/views/sample_group_views.py
+++ pangea/core/views/sample_group_views.py
@@ -223,13 +223,14 @@ def validate_sample_metadata_schema(request, pk):
     tbl = pd.DataFrame.from_dict(grp.sample_metadata(), orient='index')
     sample_names_to_inds = {i: n for i, n in enumerate(tbl.index.to_list())}
     report = frictionless.validate(tbl, schema=schema)
-    errors = report.flatten(["rowPosition", "rowName", "fieldName", "code", "message"])
+    errors = report.flatten(["rowPosition", "fieldName", "code", "message"])
     errors_remapped = []
-    for rowPos, rowName, fieldName, code, msg in errors:
+    for rowPos, fieldName, code, msg in errors:
         errors_remapped.append([sample_names_to_inds.get(rowPos, rowPos), fieldName, code, msg])
     blob = {
         'errors': errors_remapped,
         'stats': report['stats'],
+        'full_report': report,
     }
     return Response(blob)
 
