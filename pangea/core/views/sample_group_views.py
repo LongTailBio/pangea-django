89201d3c3039f14aaacfac33fe95f151922c5b3d
David Danko <dcdanko@gmail.com>
2021-05-27 10:05:03 -0400

    fix: permission issues in samples sample groups

--- pangea/core/views/sample_group_views.py
+++ pangea/core/views/sample_group_views.py
@@ -120,7 +120,7 @@ class SampleGroupSamplesView(generics.ListAPIView):
         samples = super().filter_queryset(queryset).filter(sample_groups__pk=sample_grp_uuid)
         libraries = {samp.library.group for samp in samples}
         for lib in libraries:
-            has_permission &= perm.has_object_permission(self.request, self, lib)
+            has_permission &= lib.user_can_access(self.request.user)
         if not has_permission:
             return []
         return samples.order_by('created_at')
@@ -165,21 +165,25 @@ class SampleGroupSamplesView(generics.ListAPIView):
             sample_uuids.append(sample_uuid)
 
         sample_group = SampleGroup.objects.get(pk=sample_group_uuid)
-        group_org = sample_group.organization
-        group_membership_queryset = self.request.user.organization_set.filter(pk=group_org.pk)
+        if not sample_group.user_can_access(self.request.user):
+            logger.info(
+                'attempted_delete_sample_from_group_without_permission',
+                user=request.user,
+                sample_pk=sample.pk,
+                sample_group_pk=sample_group.pk,
+            )
+            raise PermissionDenied(_('Insufficient permissions to remove sample from sample group.'))
+
         for sample_uuid in sample_uuids:
             sample = Sample.objects.get(pk=sample_uuid)
-            sample_org = sample.library.group.organization
-            sample_membership_queryset = self.request.user.organization_set.filter(pk=sample_org.pk)
-
-            if not group_membership_queryset.exists() or not sample_membership_queryset.exists():
+            if not sample.user_can_access(self.request.user):
                 logger.info(
                     'attempted_delete_sample_from_group_without_permission',
                     user=request.user,
                     sample_pk=sample.pk,
                     sample_group_pk=sample_group.pk,
                 )
-                raise PermissionDenied(_('Insufficient permissions to add sample to sample group.'))
+                raise PermissionDenied(_('Insufficient permissions to remove sample from sample group.'))
 
             sample.sample_groups.remove(sample_group)
 
